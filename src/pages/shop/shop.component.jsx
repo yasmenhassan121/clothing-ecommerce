1db66bc7b0d2f2ce2378f27490cfeaf246aa415b
yasmenhassan <yasmenah34@gmail.com>
2022-02-10 02:29:58 +0200

    add SHOP-DATA file

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SHOP_DATA from './shop.data';
 
 class ShopPage extends React.Component {
     constructor() {
         super();
-        this.state={}
+        this.state= SHOP_DATA
     }
     render() {
         return <div>hiiiii</div>
